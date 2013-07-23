0ac7b3f3b06c6a28f70239f7d9af9fa0888990fd
Alan Busby <thebusby@gmail.com>
2013-07-23 19:14:30 +0900

    Added some new examples and updated previous examples with help from Ambrose found here: https://github.com/frenchy64/type-examples/blob/master/src/type_examples/core.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.typed "0.1.17"]
+                 [com.datomic/datomic-free "0.8.4020.26"]
                  ])
--- src/type_examples/core.clj
+++ src/type_examples/core.clj
@@ -1,9 +1,11 @@
 (ns type-examples.core
   "Examples of core.typed, compiled from;
-   1. https://github.com/clojure/core.typed/wiki/Quick-Guide
-   2. http://www.clojure.net/2013/03/14/Typed-Clojure/
+   1. Much help from the esteemed Ambrose of core.typed fame.
+   2. https://github.com/clojure/core.typed/wiki/Quick-Guide
+   3. http://www.clojure.net/2013/03/14/Typed-Clojure/   
    Much thanks to both authors!"
   (:require [clojure.core.typed :as t])
+  (:use [datomic.api :only [db q] :as d])
   (:import (clojure.lang Seqable IPersistentVector Symbol Keyword ISeq Indexed)))
 
 
@@ -64,7 +66,7 @@
 (t/ann get-first-by-first (All [x] (Fn [nil -> nil] ;; Handle the case of nil
                                        [(I (clojure.lang.Seqable x) (ExactCount 0)) -> nil] ;; Handle the case of a vector of zero elements
                                        [(I (clojure.lang.Seqable x) (CountRange 1)) -> x] ;; Handle the case of a vector of one or more elements
-                                       [(t/Option (clojure.lang.Seqable x)) -> (t/Option x)] ;; Handle the case of a LazySeq, or an uncountable sequence; *NOTE* unfortunately this doesn't seem to work...
+                                       [(t/Option (clojure.lang.Seqable x)) -> (t/Option x)] ;; Handle the case of a seq where ExactCount/CountRange isn't going to work. *NOTE* this means we'll return (U nil x) instead of x or nil!!!
                                        )))
 (defn get-first-by-first [s]
   (first s))
@@ -79,16 +81,32 @@
 (t/ann get-first-by-first-test-five (U Number Keyword String))
 (def get-first-by-first-test-five (get-first-by-first [1 :two "three"]))
 
-;; Unfortunately none of the following work...
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-;; (t/ann get-first-by-first-test-six Number)
-;; (def get-first-by-first-test-six (get-first-by-first (map inc [1 2 3])))
-;; (t/ann get-first-by-first-test-seven Number)
-;; (def get-first-by-first-test-seven (get-first-by-first (mapv inc [1 2 3])))
-;; (t/ann get-first-by-first-test-eight Number)
-;; (def get-first-by-first-test-eight (get-first-by-first [1 :two "three"]))
-;; (t/ann get-first-by-first-test-nine Number)
-;; (def get-first-by-first-test-nine (get-first-by-first '(1 2 3)))
+;; map will return LazySeq which doesn't have a size, so we'll get back (t/Option Number)
+(t/ann get-first-by-first-test-six (t/Option Number)) 
+(def get-first-by-first-test-six (get-first-by-first (map inc [1 2 3])))
+
+;; As we know this can't return nil, let's help the type system understand that.
+(t/ann never-nil (All [x] [(U x nil) -> x]))
+(defn never-nil [a]
+  (assert (not (nil? a)) "Found nil where it shouldn't be possible")
+  a)
+(t/ann get-first-by-first-test-seven Number) ;; Having removed the potential nil, now we're back to just a Number
+(def get-first-by-first-test-seven (never-nil (get-first-by-first (map inc [1 2 3]))))
+
+;; Note: (assert (seq (map inc [1 2 3]))) would add (CountRange 1) to the type
+;; (t/cf (let [s (map inc [1 2 3]) _ (assert (seq s))] s)) 
+;; => (I (LazySeq t/AnyInteger) (CountRange 1))
+
+(t/ann get-first-by-first-test-eight (t/Option Number))
+(def get-first-by-first-test-eight (get-first-by-first (mapv inc [1 2 3])))
+(t/ann get-first-by-first-test-nine (t/Option Number))
+(def get-first-by-first-test-nine (get-first-by-first '(1 2 3)))
+
+;; Not sure how to make this work...
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;; (t/ann get-first-by-first-test-ten [Number -> (clojure.lang.LazySeq Number)])
+;; (defn get-first-by-first-test-ten [x] (get-first-by-first [x 2 3]))
+
 
 
 ;; Handle a seq full of something unknown, by destructuring...
@@ -183,20 +201,38 @@
 
 ;; Use HMap to indicate Clojure map types
 ;; --------------------------------------
-(t/ann handle-hmap [(HMap) -> (HMap :mandatory {:foo String})])
+(t/ann handle-hmap (Fn ['{} -> '{:foo String}]
+;;                       [(clojure.lang.APersistentMap clojure.lang.Keyword Any) -> '{:foo String}] ;; To support zipmap, but this doesn't work...
+                       ))
+;; or (t/ann handle-hmap [(HMap) -> (HMap :mandatory {:foo String})])
 (defn handle-hmap [map-arg]
   (assoc map-arg :foo "bar"))
-(t/ann handle-hmap-test (HMap))
+(t/ann handle-hmap-test '{:foo String})
 (def handle-hmap-test (handle-hmap {}))
-(t/ann handle-hmap-test-two (HMap))
-(def handle-hmap-test-two (handle-hmap {:a 1 :b 2 :c 3}))
+(t/ann handle-hmap-test-two '{:foo String})
+(def handle-hmap-test-two (handle-hmap {:foo 3.14}))
+(t/ann handle-hmap-test-three '{:foo String})
+(def handle-hmap-test-three (handle-hmap {:a 1 :b 2 :c 3}))
 
-;; This doesn't work unfortunately
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-;; (t/ann handle-hmap-test-three (HMap))
-;; (def handle-hmap-test-three (handle-hmap (zipmap [:a :b :c] 
-;;                                                  [1 2 3])))
 
+;; Unfortunately not all maps can be HMaps
+;; ---------------------------------------
+(t/ann handle-map [(clojure.lang.IPersistentMap Keyword Any) -> (clojure.lang.IPersistentMap Keyword Any)])
+(defn handle-map [m]
+  (reduce (t/ann-form #(assoc %1 %2 :win) 
+                      [(clojure.lang.IPersistentMap Keyword Any) clojure.lang.Keyword 
+                       -> 
+                       (clojure.lang.IPersistentMap Keyword Any)]) 
+          m 
+          [:a :b :c]))
+(t/ann handle-map-test (clojure.lang.IPersistentMap Keyword Any))
+(def handle-map-test (handle-map {}))
+(t/ann handle-map-test-two (clojure.lang.IPersistentMap Keyword Any))
+(def handle-map-test-two (handle-map {:a 3.14}))
+
+;; Note zipmap returns an APersistentMap, so it can't be used with an HMap
+(t/ann handle-map-test-three (clojure.lang.IPersistentMap Keyword Any))
+(def handle-map-test-three (handle-map (zipmap [:x :y :z] [1 2 3])))
 
 
 ;; How to handle a function as an argument
@@ -218,7 +254,58 @@
 
 
 ;; This is what an error looks like
-;; --------------------------------------
+;; --------------------------------
 ;; (t/ann error-case [ -> (HMap)])
 ;; (defn error-case [] (handle-hmap 1))
 
+
+
+;; How you might wrap a third party library
+;; ----------------------------------------
+(t/ann conn-uri String)
+(def conn-uri "datomic:mem://in-mem-db")
+
+;; Why does this still return a warning?
+(t/ann datomic.api/delete-database [String -> Boolean])
+(t/ann datomic.api/create-database [String -> Boolean])
+
+
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;; How would the following work with opaque types?
+;; I've replaced the actual types with Any below, 
+;; because the actual types as indicated by (class conn)
+;; don't appear to work with core.typed's annotations.
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+(t/ann datomic.api/connect [String -> Any]) ;; This should really be datomic.peer.LocalConnection instead of Any
+(t/ann conn Any) ;; This should really be datomic.peer.LocalConnection instead of Any
+(def conn (do
+            (d/delete-database conn-uri)
+            (d/create-database conn-uri)
+            (d/connect conn-uri)))
+
+(t/ann datomic.api/transact [Any (Seqable Any) -> Any]) ;; should return a future instead of Any
+
+(t/ann transact-one Any) ;; This should be a future instead of Any
+(def transact-one (d/transact conn [{:db/id                 #db/id[:db.part/db]
+                                     :db/ident              :foo/name
+                                     :db/valueType          :db.type/string
+                                     :db/cardinality        :db.cardinality/one
+                                     :db.install/_attribute :db.part/db}]))
+
+(t/ann transact-two Any) ;; This should be a future instead of Any
+(def transact-two (d/transact conn [{:db/id #db/id[:db.part/user] :foo/name "bar"}]))
+
+
+(t/ann datomic.api/q [(Seqable Any) Any -> java.util.HashSet]) ;; How do I indicated this is a HashSet of clojure.lang.PersistentVector's ?
+(t/ann datomic.api/db [Any -> Any]) ;; This should be datomic.peer.LocalConnection and datomic.db.Db instead of Any
+(t/ann query-result java.util.HashSet)
+(def query-result (q '[:find ?e ?name 
+                       :where [?e :foo/name ?name]] 
+                     (db conn)))
+
+
+;; Is there a way to get the following to work?
+;; Note: query-result is a java.util.HashSet
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;; (t/ann query-result-rec (t/Option (clojure.lang.PersistentVector Any)))
+;; (def query-result-rec (first (seq query-result)))
